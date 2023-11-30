2ca36f367bf978d2697715990ed9d409d7ee44b1
Rahul kumar <42943345+rahulchandra195@users.noreply.github.com>
2023-11-30 15:42:33 +0530

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,8 @@
+/*
+ How this obtained ? ->see the readme.md file 
+*/
+const CLIENT_ID = "Your Client Id";
+const CLEINT_SECRET = "Your Client Secret";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = "Your Refresh Token";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
